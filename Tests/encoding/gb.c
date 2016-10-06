7b65d1fa4cf3e71e5daf7a41997bd7b75ba9a3ea
ekd123 <crtmike@gmail.com>
2016-10-06 13:21:12 +0800

    encoding fun

---
+++ Tests/encoding/gb.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%c%c\n", -70, -61);
+    return 0;
+}
---
+++ Tests/encoding/utf8.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%c%c%c\n", -27, -91, -67);
+    return 0;
+}
